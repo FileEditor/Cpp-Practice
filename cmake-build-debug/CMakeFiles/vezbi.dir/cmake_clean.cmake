e7d3600c044d22b1893bd9cc495f5ff4841579a1
FileEditor <draganmatov@gmail.com>
2024-08-15 17:50:36 +0200

    Test from my (home) PC

---
+++ cmake-build-debug/CMakeFiles/vezbi.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vezbi.dir/main.cpp.obj"
+  "CMakeFiles/vezbi.dir/main.cpp.obj.d"
+  "libvezbi.dll.a"
+  "vezbi.exe"
+  "vezbi.exe.manifest"
+  "vezbi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vezbi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

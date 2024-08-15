a6d7ddf5b5c90cc8813647e12471d40d4f54cf5b
CompEng <draganmatov@gmail.com>
2024-08-15 14:40:25 +0200

    test

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
-#include <algorithm>
+#include <cctype>
 #include <iostream>
+#include <string.h>
 using namespace std;
-
+class Image {
+protected:
+    int height,width;
+    char type[5];
+public:
+    Image() {
+        this->height=this->width=0;
+        strcpy(this->type,".jpg");
+    }
+    Image(int height, int width,char *type) {
+        this->height=height;
+        this->width=width;
+        strcpy(this->type,type);
+    }
+    Image(const Image& other) {
+        this->height=other.height;
+        this->width=other.width;
+        strcpy(this->type,other.type);
+    }
+    ~Image(){}
+};
 int main() {
     cout << "test" << endl;
     return 0;
